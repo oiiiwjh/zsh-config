ad842425e106941cccbb3609fa238e17661b84de
jiahwang <87355388+oiiiwjh@users.noreply.github.com>
2025-08-03 12:41:38 +0800

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -18,3 +18,7 @@ conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/
 conda config --set show_channel_urls yes
 
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
+
+# git config
+cp ~/.gitconfig ~/.gitconfig-bak
+cp .gitconfig ~/.gitconfig
